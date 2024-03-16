e3c9d2020336550eff6956b81fe99fa64a2372ce
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-03-17 02:14:57 +0530

    profileView done

--- UniVerse_backend/post/api.py
+++ UniVerse_backend/post/api.py
@@ -6,6 +6,8 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.response import Response
 from .forms import PostForm
+from account.serializers import UserSerializer
+from account.models import User
 
 
 @api_view(['get'])
@@ -13,6 +15,15 @@ def post_list(request):
     post=Post.objects.all()
     serilaizer=PostSerializer(post,many=True)
     return JsonResponse(serilaizer.data,safe=False)
+
+@api_view(['get'])
+def profile_list(request,id):
+    user=User.objects.get(pk=id)
+    posts = Post.objects.filter(created_by_id=id)
+    print(posts)
+    post_serialaizer=PostSerializer(posts,many=True)
+    user_serializer=UserSerializer(user)
+    return JsonResponse({'posts':post_serialaizer.data,'user':user_serializer.data},safe=False)
 
 
 @api_view(['POST'])
