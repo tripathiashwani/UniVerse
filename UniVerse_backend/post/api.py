601b31c6740c0d9a2ff1a1a8d428f1c06b505766
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-04-05 02:14:06 +0530

    feed fed by friends,working of ppl you may know

--- UniVerse_backend/post/api.py
+++ UniVerse_backend/post/api.py
@@ -2,13 +2,13 @@ from django.db.models import Q
 from django.http import JsonResponse
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
 from rest_framework.views import APIView
-from .models import Post,Like,Report
+from .models import Post,Like,Report,Comment
 from .serializers import PostSerializer
 from rest_framework.response import Response
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from account.serializers import UserSerializer
 # from notification.utils import create_notification
-from account.models import User
+from account.models import User, FriendshipRequest
 
 
 @api_view(['get'])
@@ -19,12 +19,51 @@ def post_list(request):
     return JsonResponse(serilaizer.data,safe=False)
 
 @api_view(['get'])
-def profile_list(request,id):
-    user=User.objects.get(pk=id)
+def post_list_friends(request):
+    user_ids = [request.user.id]
+
+    for user in request.user.friends.all():
+        user_ids.append(user.id)
+
+    posts = Post.objects.filter(created_by_id__in=list(user_ids))
+
+    trend = request.GET.get('trend', '')
+
+    if trend:
+        posts = posts.filter(body__icontains='#' + trend).filter(is_private=False)
+
+    serializer = PostSerializer(posts, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def post_list_profile(request, id):   
+    user = User.objects.get(pk=id)
     posts = Post.objects.filter(created_by_id=id)
-    post_serialaizer=PostSerializer(posts,many=True)
-    user_serializer=UserSerializer(user)
-    return JsonResponse({'posts':post_serialaizer.data,'user':user_serializer.data},safe=False)
+
+    # if not request.user in user.friends.all():
+    #     posts = posts.filter(is_private=False)
+
+    posts_serializer = PostSerializer(posts, many=True)
+    user_serializer = UserSerializer(user)
+
+    can_send_friendship_request = True
+
+    if request.user in user.friends.all():
+        can_send_friendship_request = False
+    
+    check1 = FriendshipRequest.objects.filter(created_for=request.user).filter(created_by=user)
+    check2 = FriendshipRequest.objects.filter(created_for=user).filter(created_by=request.user)
+
+    if check1 or check2:
+        can_send_friendship_request = False
+
+    return JsonResponse({
+        'posts': posts_serializer.data,
+        'user': user_serializer.data,
+        'can_send_friendship_request': can_send_friendship_request
+    }, safe=False)
 
 
 @api_view(['POST'])
@@ -63,18 +102,17 @@ def post_like(request, pk):
 @api_view(['POST'])
 def post_report(request, pk):
     post = Post.objects.get(pk=pk)
+    count=0
     if not post.reports.filter(created_by=request.user):
         report = Report.objects.create(created_by=request.user)
         post = Post.objects.get(pk=pk)
         post.reports_count = post.reports_count + 1
+        count=print(post.reports_count,"report_countiiiii")
         post.reports.add(report)
         post.save()
-
-
         # notification = create_notification(request, 'post_reported', post_id=post.id)
-        
-
         return JsonResponse({'message': 'reported'})
+    print(count)
     return JsonResponse({'message':'already reported'})
 
 @api_view(['DELETE'])
@@ -84,19 +122,18 @@ def post_delete(request, pk):
 
     return JsonResponse({'message': 'post deleted'})
 
-# @api_view(['POST'])
-# def post_like(request, pk):
-#     post = Post.objects.get(pk=pk)
 
-#     try:
-#         like = Like.objects.get(created_by=request.user)
-#         like.delete()
-#         post.likes_count -= 1
-#         post.save()
-#         return JsonResponse({'message': 'disliked'})
-#     except Like.DoesNotExist:
-#         like = Like.objects.create(created_by=request.user)
-#         post.likes_count += 1
-#         post.likes.add(like)
-#         post.save()
-#         return JsonResponse({'message': 'liked'})
+@api_view(['POST'])
+def create_comment(request,pk):
+    form=CommentForm(request.data)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.created_by = request.user
+        comment.save()
+        post=Post.objects.filter(pk=pk)
+        post.comments_count = post.comments_count + 1
+        post.comments.add(comment)
+        post.save()
+        serilaizer=PostSerializer(comment)
+        return JsonResponse(serilaizer.data,safe=False)
+    return JsonResponse({'data':'msg from back'})
--- UniVerse_backend/post/urls.py
+++ UniVerse_backend/post/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import api
 print("reached at post.url")
 urlpatterns = [
-    path('', api.post_list, name='post_list'),
+    path('', api.post_list_friends, name='post_list'),
     path('create/', api.create_post, name='create_post'),
-    path('profile/<uuid:id>/', api.profile_list, name='profile_list'),
+    path('profile/<uuid:id>/', api.post_list_profile, name='profile_list'),
     path('<uuid:pk>/like/', api.post_like, name='post_like'),
     path('<uuid:pk>/report/', api.post_report, name='post_like'),
     path('<uuid:pk>/delete/', api.post_delete, name='post_like'),
