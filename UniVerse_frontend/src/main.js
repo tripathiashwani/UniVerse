65ffb76f11fc1b747e3c295c984f6128899a865a
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-07-04 01:09:18 +0530

    updated port number

--- UniVerse_frontend/src/main.js
+++ UniVerse_frontend/src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import axios from 'axios'
 
 const app = createApp(App)
-axios.defaults.baseURL='http://127.0.0.1:8000'
+axios.defaults.baseURL='http://13.60.81.46:80/'
 
 app.use(createPinia())
 app.use(router,axios)
