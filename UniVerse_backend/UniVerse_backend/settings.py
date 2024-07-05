f4d790d7c9821e1954b595b3474ee3d3b350a6ff
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-07-05 19:37:56 +0530

    cors settings

--- UniVerse_backend/UniVerse_backend/settings.py
+++ UniVerse_backend/UniVerse_backend/settings.py
@@ -40,7 +40,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "*"
+    "https://*",
+    "http://*"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
