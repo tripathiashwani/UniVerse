aa5ac586ee71997adef70639b6c128e30cdcdfd1
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-07-25 22:56:55 +0530

    channels+saving+retrieving  chats done

---
+++ UniVerse_backend/chat/api.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from django.conf import settings
+from django.contrib.auth.forms import PasswordChangeForm
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from account.models import User
+# Your code here
+from notification.utils import create_notification
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from chat.models import ConversationMessage, Conversation
+from chat.serializers import MessageSerializer,ConversationSerializer
+
+# @api_view(['GET'])
+# def Get_Messages(request):
+#     messages = Message.objects.all()
+#     serializer = MessageSerializer(messages, many=True)
+#     return JsonResponse(serializer.data, safe=False)
+
+def create_message(message, sender, room_name):
+    conversation, created = Conversation.objects.get_or_create(room=room_name)
+    user = User.objects.get(name=sender)  # Or use a different method to get the user
+
+    ConversationMessage.objects.create(
+        conversation=conversation,
+        body=message,
+        sent_to=conversation.users.exclude(name=sender).first(),  # Assuming the conversation has only 2 users
+        created_by=user,
+    )
+
+@api_view(['GET'])
+def Get_Room(request, room):
+    print("----->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    messages = Conversation.objects.filter(room=room)  # Use filter instead of get
+    print(messages)
+    serializer = ConversationSerializer(messages, many=True)
+    print(serializer.data)
+    return JsonResponse(serializer.data[0], safe=False)
--- UniVerse_backend/chat/models.py
+++ UniVerse_backend/chat/models.py
@@ -1,13 +1,11 @@
 import uuid
-
 from django.db import models
 from django.utils.timesince import timesince
-
 from account.models import User
 
 
 class Conversation(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    room = models.CharField(primary_key=True, max_length=500, editable=False, null=False)
     users = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -19,10 +17,10 @@ class Conversation(models.Model):
 class ConversationMessage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
-    body = models.TextField()
+    message = models.TextField()
     sent_to = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     
     def created_at_formatted(self):
        return timesince(self.created_at)
